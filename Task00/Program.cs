5b003691bc729ed01b64d066140a03f15e9d7eb9
Ya <gileva.anastasia@bk.ru>
2023-09-22 20:51:01 +0500

    Выполнено задание №0

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+
+Console.WriteLine("введите целое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+int square = numberOne * numberOne;
+Console.WriteLine( $" {numberOne} -> {square} ");
