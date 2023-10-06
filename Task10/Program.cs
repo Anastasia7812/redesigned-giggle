bd6caf0996ce108895df45de425187678b719f6e
Ya <gileva.anastasia@bk.ru>
2023-10-06 20:00:14 +0500

    Добавлено задание 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("введите трехзначное целое число ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondRank = number / 10 % 10;
+
+Console.Write ($"Вторая цифра `{secondRank}`");
