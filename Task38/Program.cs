025603861eae89c45dc3c11c431136aa7e4b8d2d
Ya <gileva.anastasia@bk.ru>
2023-10-08 19:13:40 +0500

    Добавлено задание 38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+Console.WriteLine("Введите размер массива");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+
+PrintArray(numbers);
+
+int max = numbers[0];
+
+int min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+
+{
+    if (numbers[i] > max)
+
+    {
+        max = numbers[i];
+    }
+
+    else if (numbers[i] < min)
+
+    {
+        min = numbers[i];
+    }
+
+}
+
+Console.WriteLine($"Минимальное число: {min}");
+
+Console.WriteLine($"Минимальное число: {max}");
+
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+void FillArrayRandomNumbers(int[] array)
+
+{
+
+    for (int i = 0; i < array.Length; i++)
+
+    {
+        array[i] = new Random().Next(1, 555);
+    }
+
+}
+
+void PrintArray(int[] array)
+
+{
+
+    for (int i = 0; i < array.Length; i++)
+
+    {
+        Console.Write(array[i] + " ");
+    }
+
+    Console.WriteLine();
+
+}
